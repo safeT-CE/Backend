9869f66aede97430d5bf80d938e66cb1991e5920
19013na <19013@naver.com>
2024-07-19 17:08:02 +0900

    Fix : 앱으로 면허증 - 얼굴 동일성 인식 코드 수정

--- src/main/java/com/example/kickboard/kickboard/repository/DataRecordRepository.java
+++ src/main/java/com/example/kickboard/kickboard/repository/DataRecordRepository.java
@@ -5,8 +5,6 @@ import com.example.kickboard.kickboard.entity.DataRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface DataRecordRepository extends JpaRepository<DataRecord, String> {
 }
--- src/main/java/com/example/kickboard/kickboard/service/CSVService.java
+++ src/main/java/com/example/kickboard/kickboard/service/CSVService.java
@@ -10,9 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
 
 @Service
 public class CSVService {
