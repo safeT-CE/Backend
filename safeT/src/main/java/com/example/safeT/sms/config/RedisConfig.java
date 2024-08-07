6cf362347222695c14dac79c0b0845eb7f718fb9
hj <hynj111nnn@gmail.com>
2024-08-07 16:02:02 +0900

    Feat : 회원 가입 SMS 본인 인증

---
+++ safeT/src/main/java/com/example/safeT/sms/config/RedisConfig.java
@@ -0,0 +1,33 @@
+// redis 연동 클래스
+
+package com.example.safeT.sms.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@EnableRedisHttpSession
+@Configuration
+public class RedisConfig {
+
+    @Value("${redis.host}")
+    private String redisHost;
+    @Value("${redis.port}")
+    private int redisPort;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate() {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory());
+        return stringRedisTemplate;
+    }
+}
---
+++ safeT/src/main/java/com/example/safeT/sms/controller/SmsController.java
@@ -0,0 +1,43 @@
+// sms 인증 기능 처리를 위한 컨트롤러 클래스
+
+package com.example.safeT.sms.controller;
+
+import com.example.safeT.sms.dto.base.DefaultRes;
+import com.example.safeT.sms.dto.UserRequest;
+import com.example.safeT.sms.exception.CustomExceptions;
+import com.example.safeT.sms.exception.ResponseMessage;
+import com.example.safeT.sms.exception.StatusCode;
+import com.example.safeT.sms.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/sms-certification")
+public class SmsController extends BaseController {
+    private final UserService userService;
+
+    // 인증 번호 전송
+    @PostMapping("/send")
+    public ResponseEntity<?> sendSms(@RequestBody UserRequest.SmsCertificationRequest requestDto) throws Exception {
+        try {
+            userService.sendSms(requestDto);
+            return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.SMS_CERT_MESSAGE_SUCCESS), HttpStatus.OK); // sms 인증 번호 전송 성공
+        } catch (CustomExceptions.Exception e) {
+            return handleApiException(e, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // 인증 번호 확인
+    @PostMapping("/confirm")
+    public ResponseEntity<Void> SmsVerification(@RequestBody UserRequest.SmsCertificationRequest requestDto) throws Exception{
+        try {
+            userService.verifySms(requestDto);
+            return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.SMS_CERT_SUCCESS), HttpStatus.OK); // sms 인증 번호 확인 완료 (본인 인증 성공)
+        } catch (CustomExceptions.Exception e) {
+            return handleApiException(e, HttpStatus.BAD_REQUEST);
+        }
+    }
+}
