3adefb2a8f4fc508319423b9a847a5fe9611f085
19013na <19013@naver.com>
2024-08-09 00:04:09 +0900

    Feat : 면허증 - 얼굴 동일성 판단 후 AWS S3에 저장된 CSV 경로 User.identity에 저장

--- src/main/java/com/example/kickboard/kickboard/controller/FaceController.java
+++ src/main/java/com/example/kickboard/kickboard/controller/FaceController.java
@@ -1,8 +1,7 @@
 package com.example.kickboard.kickboard.controller;
 
-
+import com.example.kickboard.kickboard.dto.FaceRequest;
 import com.example.kickboard.kickboard.service.FaceService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,10 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
@@ -60,37 +57,19 @@ public class FaceController {
             pb.redirectErrorStream(true);
 
             Process process = pb.start();
-
-
-            // Python 스크립트의 출력 읽기
-            StringBuilder output = new StringBuilder();
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    output.append(line).append("\n");
-                    //output.append(line);
-                }
-            }
-
             int exitCode =  process.waitFor();
 
+            // 얼굴, 신분증 사진 삭제
             Files.delete(licenseFile.toPath());
             Files.delete(faceFile.toPath());
 
             if(exitCode == 0){
-                String identity = output.toString().trim();
-
-                // 응답
                 response.put("message", "success");
-                response.put("identity", identity);
-                faceService.saveIdentity(userId, identity);
-
                 return new ResponseEntity<>(response, HttpStatus.OK);
             } else{
                 response.put("message", "Can't store the CSV file");
                 return new ResponseEntity<>(response, HttpStatus.NOT_MODIFIED);
             }
-
         } catch (IOException | InterruptedException e){
             e.printStackTrace();
             response.put("message", "Exception occurred : " + e.getMessage());
@@ -98,9 +77,9 @@ public class FaceController {
         }
     }
 
-    // 테스트용
+    // 테스트용 [앱에 자신의 면허증 - 얼굴 등록]
     @GetMapping("/upload/test")
-    public <T> ResponseEntity<Map<String, Object>> uploadTestFile(
+    public <T> ResponseEntity<Map<String, Object>> startTestFile(
             @RequestParam("userId") Long userId){
         Map<String, Object> response = new HashMap<>();
 
@@ -109,51 +88,43 @@ public class FaceController {
             temDir = Files.createTempDirectory("uploads");
         }catch (IOException e){
             e.printStackTrace();
-            //response.put("message", "not found" + licenseImage + faceImage + "and temDir");
             return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
         }
 
-        // 면허증, 얼굴 사진 저장
+        // 면허증, 얼굴 동일성 확인
         try{
             ProcessBuilder pb = new ProcessBuilder("python", "C:/Users/SAMSUNG/Desktop/detection/Detection/test.py",
                     String.valueOf(userId));
 
             pb.redirectErrorStream(true);
-
             Process process = pb.start();
-
-            // Python 스크립트의 출력 읽기
-            StringBuilder output = new StringBuilder();
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    output.append(line); //.append("\n");
-                    //output.append(line);
-                }
-            }
-
+            
             int exitCode =  process.waitFor();
             if(exitCode == 0){
-                String identity = output.toString().trim();
-                log.info("FaceController : 1"); //
-                log.info("FaceController : " + identity);
-
-                // 처리된 JSON 응답
                 response.put("message", "success");
-                response.put("identity", identity);
-                faceService.saveIdentity(userId, identity);  // 저장 로직에 JSON 응답 전달
-                log.info("FaceController : 2"); //
-
                 return new ResponseEntity<>(response, HttpStatus.OK);
             } else{
                 response.put("message", "Can't store the CSV file");
                 return new ResponseEntity<>(response, HttpStatus.NOT_MODIFIED);
             }
-
         } catch (IOException | InterruptedException e){
             e.printStackTrace();
             response.put("message", "Exception occurred : " + e.getMessage());
             return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // AWS S3에 저장된 csv 경로 받기
+    @PostMapping("/upload")
+    public ResponseEntity<Map<String, Object>>  uploadTestFile(@RequestParam("userId") Long userId, @RequestBody FaceRequest request){
+        try{
+            faceService.saveIdentity(userId, request);
+            Map<String, Object> response = new HashMap<>();
+            response.put("userId", request.getUserId());
+            response.put("identity", request.getIdentity());
+            return ResponseEntity.ok(response);
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.status(500).body(Map.of("error", "Failed to register Identity to " + userId));
         }
     }
 }
--- src/main/java/com/example/kickboard/kickboard/service/FaceService.java
+++ src/main/java/com/example/kickboard/kickboard/service/FaceService.java
@@ -1,14 +1,11 @@
 package com.example.kickboard.kickboard.service;
 
-
+import com.example.kickboard.kickboard.dto.FaceRequest;
+import com.example.kickboard.kickboard.exception.UserIdNotFoundException;
 import com.example.kickboard.login.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.io.IOException;
-import java.util.Map;
-
 
 @Service
 public class FaceService {
@@ -16,16 +13,13 @@ public class FaceService {
     @Autowired
     private UserRepository userRepository;
 
-    // CSV 파일을 데이터베이스에 저장하는 메서드
-    public void saveCSVToDatabase(String csvFilePath, Long userId) throws IOException {
-        // userRepository의 userId에 맞는 db identity에 csvFilePath 저장
-    }
-
     @Transactional
-    public void saveIdentity(Long userId, String identity) {
-        // userId로 찾은 user의 idenetity 데이터를 넣기
-        userRepository.updateIdentityStatus(userId, identity);
-
-
+    public void saveIdentity(Long userId, FaceRequest request) {
+        if (userId == request.getUserId()) {
+            // userId로 찾은 user의 idenetity 데이터를 넣기
+            userRepository.updateIdentityStatus(userId, request.getIdentity());
+        } else {
+            throw new UserIdNotFoundException("The UserId in the request doesn't match the UserId in the response.");
+        }
     }
 }
