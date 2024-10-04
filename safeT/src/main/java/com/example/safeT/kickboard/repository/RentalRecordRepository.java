006f0f3c80f8666d46a0b6707f89c4b0504735c4
hj <hynj111nnn@gmail.com>
2024-10-05 07:10:02 +0900

    Refactor: 반납하기 기능 수정

--- safeT/src/main/java/com/example/safeT/kickboard/repository/RentalRecordRepository.java
+++ safeT/src/main/java/com/example/safeT/kickboard/repository/RentalRecordRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 // 킥보드 ID로 대여 기록 조회 -> 대여 기록 관리
-public interface RentalRepository extends JpaRepository<Rental, Long> {
+public interface RentalRecordRepository extends JpaRepository<Rental, Long> {
     Optional<Rental> findByKickboardId(Long kickboardId);
     Optional<Rental> findTopByKickboardIdAndReturnedFalseOrderByRentedAtDesc(Long kickboardId);
 }
