2dfb03b798397610895f6150659085e6dbff59b9
hj <hynj111nnn@gmail.com>
2024-10-08 03:17:02 +0900

    Chore: Rental 엔티티 필드 이름 수정

--- safeT/src/main/java/com/example/safeT/kickboard/entity/Rental.java
+++ safeT/src/main/java/com/example/safeT/kickboard/entity/Rental.java
@@ -23,7 +23,7 @@ public class Rental {
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    private Long userId;
 
     @Column(name = "kickboard_id", nullable = false)
     private Long kickboardId;
